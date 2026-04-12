dd0be2bb00018a0538e108f70add17e0a4a61928
mmahattan <razerearbuds@gmail.com>
2026-04-12 04:10:17 -0700

    feat: add Sapporo 2016

--- src/data/places.ts
+++ src/data/places.ts
@@ -28,7 +28,8 @@ export const places: Place[] = [
     country: "Japan",
     isoNumeric: "392",
     cities: [
-      { city: "Tokyo", lat: 35.6762, lon: 139.6503, dates: "2006, 2014, 2016, 2017, 2019", weight: 4 },
+      { city: "Tokyo",   lat: 35.6762, lon: 139.6503, dates: "2006, 2014, 2016, 2017, 2019", weight: 4 },
+      { city: "Sapporo", lat: 43.0618, lon: 141.3545, dates: "2016", weight: 1 },
     ],
   },
   {
